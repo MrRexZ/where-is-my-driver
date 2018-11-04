a4275a5b7bc08d0fc8100dda331885f22ae3aa57
Anthony Tjuatja <anthonytjuatja@gmail.com>
2018-11-04 13:39:10 +0700

    Added driver usecase interface

--- pkg/driver/usecase.go
+++ pkg/driver/usecase.go
@@ -1,1 +1,8 @@
 package driver
+
+import "gojek-1st/pkg/entity"
+
+type Usecase interface {
+	UpdateLocation(id string) (status string, err error)
+	FindDrivers(latitude float64, longitude float64, radius float64, limit int8) (status string, err error, drivers []*entity.Driver)
+}
