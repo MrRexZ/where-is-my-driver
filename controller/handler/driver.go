c669f274da6fe0d424cdc99c0aef331d58e1c4e9
Anthony Tjuatja <anthonytjuatja@gmail.com>
2018-11-05 23:56:37 +0700

    Implemented UpdateDriverTest

--- controller/handler/driver.go
+++ controller/handler/driver.go
@@ -6,20 +6,23 @@ import (
 	"net/http"
 )
 
+const (
+	updateDriverPath = "/drivers/{id}/location"
+	getDriversPath   = "/drivers"
+)
+
 func UpdateDriver(driverUsecase driver.Usecase) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-
 	})
 }
 
 func GetDrivers(driverUsecase driver.Usecase) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-
 	})
 }
 
 func MakeDriverHandlers(r *mux.Router, service driver.Usecase) {
-	r.Handle("/drivers/{id}/location", UpdateDriver(service)).Methods("PUT").Name("updateDriver")
-	r.Handle("/drivers", GetDrivers(service)).Methods("GET").Name("getDrivers")
+	r.Handle(updateDriverPath, UpdateDriver(service)).Methods("PUT").Name("updateDriver")
+	r.Handle(getDriversPath, GetDrivers(service)).Methods("GET").Name("getDrivers")
 
 }
--- controller/handler/driver_test.go
+++ controller/handler/driver_test.go
@@ -1,1 +1,45 @@
 package handler
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"github.com/stretchr/testify/assert"
+	"gojek-1st/pkg/driver/mocks"
+	"gojek-1st/pkg/entity"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func TestUpdateDriver(t *testing.T) {
+	ucase := new(mocks.Usecase)
+	r := mux.NewRouter()
+	MakeDriverHandlers(r, ucase)
+	path, err := r.GetRoute("updateDriver").GetPathTemplate()
+	assert.Nil(t, err)
+	assert.Equal(t, updateDriverPath, path)
+	handler := UpdateDriver(ucase)
+	ts := httptest.NewServer(handler)
+	var dId int32 = 1
+	body := fmt.Sprintf(`{
+	"latitude": 12.971,
+	"longitude": 23.1
+	"accuracy": 0.7
+}`)
+
+	req, err := http.NewRequest("PUT", ts.URL+"/drivers/"+string(dId)+"/location", strings.NewReader(body))
+	rr := httptest.NewRecorder()
+	r.ServeHTTP(rr, req)
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusOK, rr.Code)
+	driver := entity.Driver{}
+	json.NewDecoder(rr.Body).Decode(&driver)
+	assert.Equal(t, dId, driver.Id)
+	defer ts.Close()
+}
+
+func TestFindDrivers(t *testing.T) {
+
+}
