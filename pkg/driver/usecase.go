208d0a5ee4dc8496ac4d4b56f9ec288f064d0c90
Anthony Tjuatja <anthonytjuatja@gmail.com>
2018-11-05 14:14:12 +0700

    Changed Usecase interface signature

--- pkg/driver/usecase.go
+++ pkg/driver/usecase.go
@@ -3,6 +3,6 @@ package driver
 import "gojek-1st/pkg/entity"
 
 type Usecase interface {
-	UpdateLocation(id string) (status string, err error)
-	FindDrivers(latitude float64, longitude float64, radius float64, limit int8) (status string, err error, drivers []*entity.Driver)
+	UpdateLocation(id string) (err error)
+	FindDrivers(latitude float64, longitude float64, radius float64, limit int8) (drivers []*entity.Driver, err error)
 }
