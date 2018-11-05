36673cbf226338bae27510a506606255b13d702c
Anthony Tjuatja <anthonytjuatja@gmail.com>
2018-11-06 00:32:06 +0700

    Fixed int32 to str conversion when calling endpoint, fixed missing comma, implemented mock

--- controller/handler/driver_test.go
+++ controller/handler/driver_test.go
@@ -5,31 +5,35 @@ import (
 	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 	"gojek-1st/pkg/driver/mocks"
 	"gojek-1st/pkg/entity"
 	"net/http"
 	"net/http/httptest"
+	"strconv"
 	"strings"
 	"testing"
 )
 
 func TestUpdateDriver(t *testing.T) {
+
+	var dId int32 = 1
 	ucase := new(mocks.Usecase)
 	r := mux.NewRouter()
+	ucase.On("UpdateLocation", dId, mock.Anything, mock.Anything, mock.Anything).Return(dId, nil)
 	MakeDriverHandlers(r, ucase)
 	path, err := r.GetRoute("updateDriver").GetPathTemplate()
 	assert.Nil(t, err)
 	assert.Equal(t, updateDriverPath, path)
 	handler := UpdateDriver(ucase)
 	ts := httptest.NewServer(handler)
-	var dId int32 = 1
 	body := fmt.Sprintf(`{
 	"latitude": 12.971,
-	"longitude": 23.1
+	"longitude": 23.1,
 	"accuracy": 0.7
 }`)
 
-	req, err := http.NewRequest("PUT", ts.URL+"/drivers/"+string(dId)+"/location", strings.NewReader(body))
+	req, err := http.NewRequest("PUT", ts.URL+"/drivers/"+strconv.Itoa(int(dId))+"/location", strings.NewReader(body))
 	rr := httptest.NewRecorder()
 	r.ServeHTTP(rr, req)
 	assert.NoError(t, err)
