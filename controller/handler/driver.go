bb9c479adc7302428ead58949696678ee840f487
Anthony Tjuatja <anthonytjuatja@gmail.com>
2018-11-06 00:41:37 +0700

    Rename GetDrivers to FindDrivers

--- controller/handler/driver.go
+++ controller/handler/driver.go
@@ -54,13 +54,13 @@ func writeError(w *http.ResponseWriter, error error, errorTag string) {
 	return
 }
 
-func GetDrivers(driverUsecase driver.Usecase) http.Handler {
+func FindDrivers(driverUsecase driver.Usecase) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 	})
 }
 
 func MakeDriverHandlers(r *mux.Router, service driver.Usecase) {
 	r.Handle(updateDriverPath, UpdateDriver(service)).Methods("PUT").Name("updateDriver")
-	r.Handle(getDriversPath, GetDrivers(service)).Methods("GET").Name("getDrivers")
+	r.Handle(getDriversPath, FindDrivers(service)).Methods("GET").Name("getDrivers")
 
 }
