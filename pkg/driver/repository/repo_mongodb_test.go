76743b20531df077fc97ea5f3e20f571570a7150
Anthony Tjuatja <anthonytjuatja@gmail.com>
2018-11-05 12:24:06 +0700

    Added test for updating driver

--- pkg/driver/repository/repo_mongodb_test.go
+++ pkg/driver/repository/repo_mongodb_test.go
@@ -14,17 +14,33 @@ const (
 
 func Test_DriverService(t *testing.T) {
 	t.Run("CreateDriver", createDriver_should_insert_correctly)
+	t.Run("UpdateDriver", createDriverAndUpdate_should_update_correctly)
 }
 
-func createDriver_should_insert_correctly(t *testing.T) {
-	client, err := CreateMongoClient(mongoUrl)
-	mongoRepository := CreateMongoRepository(client, dbName)
-	driver := entity.Driver{
+func CreateTestDriver1() entity.Driver {
+	test_driver_1 := entity.Driver{
 		Accuracy: 0.7,
 		Lat:      12.3,
 		Long:     23.1,
 		Id:       2,
 	}
+	return test_driver_1
+}
+
+func UpdatedTestDriver1() entity.Driver {
+	test_driver_1 := entity.Driver{
+		Accuracy: 0.6,
+		Lat:      2.3,
+		Long:     1.1,
+		Id:       2,
+	}
+	return test_driver_1
+}
+
+func createDriver_should_insert_correctly(t *testing.T) {
+	client, err := CreateMongoClient(mongoUrl)
+	mongoRepository := CreateMongoRepository(client, dbName)
+	driver := CreateTestDriver1()
 
 	_, err = mongoRepository.Store(&driver)
 
@@ -49,6 +65,46 @@ func createDriver_should_insert_correctly(t *testing.T) {
 	}
 	if results[0].Id != driver.Id {
 		t.Error("Incorrect Id. Expected `%i`, Got: `%i`", driver.Id, results[0].Id)
+	}
+
+	defer func() {
+		DropDatabase(client, dbName)
+	}()
+}
+
+func createDriverAndUpdate_should_update_correctly(t *testing.T) {
+	client, err := CreateMongoClient(mongoUrl)
+	mongoRepository := CreateMongoRepository(client, dbName)
+	driver := CreateTestDriver1()
+	updatedDriver := UpdatedTestDriver1()
+
+	_, err = mongoRepository.Store(&driver)
+	_, err = mongoRepository.Store(&updatedDriver)
+
+	if err != nil {
+		t.Errorf("Unable to create driver: %s", err.Error())
+	}
+	var results []entity.Driver
+	cursor, err := client.Database(dbName).Collection(collectionName).Find(context.Background(), nil)
+
+	for cursor.Next(context.Background()) {
+		driver := entity.Driver{}
+		err := cursor.Decode(&driver)
+		if err != nil {
+			log.Fatal(err)
+		}
+		results = append(results, driver)
+
+	}
+	count := len(results)
+	if count != 1 {
+		t.Error("Incorrect number of results. Expected `1`, got: `%i`", count)
+	}
+	if results[0].Id != driver.Id {
+		t.Error("Incorrect Id. Expected `%i`, Got: `%i`", driver.Id, results[0].Id)
+	}
+	if results[0].Lat != updatedDriver.Lat || results[0].Long != updatedDriver.Long {
+		t.Error("Incorrect LatLong. Expected `%i`, `%i`, Got: `%i`, `%i`", updatedDriver.Lat, updatedDriver.Long, results[0].Lat, results[0].Long)
 	}
 
 	defer func() {
