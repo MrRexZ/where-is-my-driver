c22563d152395f2b6056bf57320661c31a13fb79
Anthony Tjuatja <anthonytjuatja@gmail.com>
2018-11-05 14:30:17 +0700

    Added StoreMany method to Repository interface

--- pkg/driver/repository.go
+++ pkg/driver/repository.go
@@ -3,6 +3,7 @@ package driver
 import "gojek-1st/pkg/entity"
 
 type Repository interface {
+	StoreMany(ds []*entity.Driver) error
 	Store(d *entity.Driver) (id string, err error)
 	Get(id string) (d *entity.Driver, err error)
 }
