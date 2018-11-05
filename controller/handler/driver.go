6748f4f0be9bd47e534a92e228acd1254c7e8cf7
Anthony Tjuatja <anthonytjuatja@gmail.com>
2018-11-05 22:40:06 +0700

    Changed method formatting

--- controller/handler/driver.go
+++ controller/handler/driver.go
@@ -6,20 +6,20 @@ import (
 	"net/http"
 )
 
-func updateDrivers(driverUsecase usecase.DriverUsecase) http.Handler {
+func UpdateDriver(driverUsecase usecase.DriverUsecase) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
 	})
 }
 
-func getDrivers(driverUsecase usecase.DriverUsecase) http.Handler {
+func GetDrivers(driverUsecase usecase.DriverUsecase) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
 	})
 }
 
 func MakeDriverHandlers(r *mux.Router, service usecase.DriverUsecase) {
-	r.Handle("/drivers/{id}/location", updateDrivers(service)).Methods("PUT").Name("updateDriver")
-	r.Handle("/drivers", getDrivers(service)).Methods("GET").Name("getDrivers")
+	r.Handle("/drivers/{id}/location", UpdateDriver(service)).Methods("PUT").Name("updateDriver")
+	r.Handle("/drivers", GetDrivers(service)).Methods("GET").Name("getDrivers")
 
 }
