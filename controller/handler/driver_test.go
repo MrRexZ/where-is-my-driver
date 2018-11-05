3d32e6b7a2719d9ca0785b3543c926147678ee63
Anthony Tjuatja <anthonytjuatja@gmail.com>
2018-11-06 01:52:13 +0700

    Implemented TestFindDrivers method to check for valid lat lng and renamed a method that was used for testing the update/insert of driver

--- controller/handler/driver_test.go
+++ controller/handler/driver_test.go
@@ -15,7 +15,35 @@ import (
 	"testing"
 )
 
-func TestUpdateDriver(t *testing.T) {
+func CreateTestDriver1() entity.Driver {
+	test_driver_1 := entity.Driver{
+		Accuracy: 0.7,
+		Lat:      51.506752,
+		Long:     -0.132912,
+		Id:       2,
+	}
+	return test_driver_1
+}
+
+func CreateTestDriver2() entity.Driver {
+	return entity.Driver{
+		Accuracy: 0.8,
+		Lat:      51.508888,
+		Long:     -0.125706,
+		Id:       3,
+	}
+}
+
+func CreateTestDriver3() entity.Driver {
+	return entity.Driver{
+		Accuracy: 0.7,
+		Lat:      51.508034,
+		Long:     0.084315,
+		Id:       4,
+	}
+}
+
+func TestUpdateDriver_validId_validLatLng(t *testing.T) {
 
 	var dId int32 = 1
 	ucase := new(mocks.Usecase)
@@ -44,6 +72,32 @@ func TestUpdateDriver(t *testing.T) {
 	defer ts.Close()
 }
 
-func TestFindDrivers(t *testing.T) {
+func TestFindDrivers_validLatlng(t *testing.T) {
+	user_lat := "51.507351"
+	user_long := "-0.127758"
+	driver1 := CreateTestDriver1()
+	driver2 := CreateTestDriver2()
+	expectedDrivers := []*entity.Driver{&driver1, &driver2}
+	ucase := new(mocks.Usecase)
+	ucase.On("FindDrivers", "51.507351", user_lat, user_long, mock.Anything, mock.Anything).Return(expectedDrivers, nil)
+
+	r := mux.NewRouter()
+	MakeDriverHandlers(r, ucase)
+	path, err := r.GetRoute("findDrivers").GetPathTemplate()
+	assert.NoError(t, err)
+	assert.Equal(t, findDriversPath, path)
+	ts := httptest.NewServer(r)
+	defer ts.Close()
+
+	url, err := r.Get("findDrivers").URL("latitude", user_lat, "longitude", user_long)
+	res, err := http.Get(ts.URL + url.String())
+	assert.NoError(t, err)
+	var actualDrivers []*entity.Driver
+	err = json.NewDecoder(res.Body).Decode(&actualDrivers)
+	assert.NoError(t, err)
+	assert.ElementsMatch(t, expectedDrivers, actualDrivers)
+}
+
+func TestFindDrivers_invalidLatLng(t *testing.T) {
 
 }
