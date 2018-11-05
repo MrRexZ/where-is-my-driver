394b82d320ad5483493267a0d5aba4e79b49e2af
Anthony Tjuatja <anthonytjuatja@gmail.com>
2018-11-05 22:48:21 +0700

    Fixed function parameter usecase type

--- controller/handler/driver.go
+++ controller/handler/driver.go
@@ -2,23 +2,23 @@ package handler
 
 import (
 	"github.com/gorilla/mux"
-	"gojek-1st/pkg/driver/usecase"
+	"gojek-1st/pkg/driver"
 	"net/http"
 )
 
-func UpdateDriver(driverUsecase usecase.DriverUsecase) http.Handler {
+func UpdateDriver(driverUsecase driver.Usecase) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
 	})
 }
 
-func GetDrivers(driverUsecase usecase.DriverUsecase) http.Handler {
+func GetDrivers(driverUsecase driver.Usecase) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
 	})
 }
 
-func MakeDriverHandlers(r *mux.Router, service usecase.DriverUsecase) {
+func MakeDriverHandlers(r *mux.Router, service driver.Usecase) {
 	r.Handle("/drivers/{id}/location", UpdateDriver(service)).Methods("PUT").Name("updateDriver")
 	r.Handle("/drivers", GetDrivers(service)).Methods("GET").Name("getDrivers")
 
