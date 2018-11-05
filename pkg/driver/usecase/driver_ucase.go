7598f71c41d0efed6c7c2d03228f351557ad8b45
Anthony Tjuatja <anthonytjuatja@gmail.com>
2018-11-05 16:58:17 +0700

    Added test to verify ucase for finding drivers within a certain radius

--- pkg/driver/usecase/driver_ucase.go
+++ pkg/driver/usecase/driver_ucase.go
@@ -1,1 +1,24 @@
 package usecase
+
+import (
+	"gojek-1st/pkg/driver"
+	"gojek-1st/pkg/entity"
+)
+
+type DriverUsecase struct {
+	repo driver.Repository
+}
+
+func NewDriverUsecase(repo driver.Repository) *DriverUsecase {
+	return &DriverUsecase{
+		repo: repo,
+	}
+}
+
+func (du *DriverUsecase) FindDrivers(latitude float64, longitude float64, radius float64, limit int8) (drivers []*entity.Driver, err error) {
+	return nil, nil
+}
+
+func (du *DriverUsecase) UpdateLocation(id string) (err error) {
+	return nil
+}
--- pkg/driver/usecase/driver_ucase_test.go
+++ pkg/driver/usecase/driver_ucase_test.go
@@ -1,1 +1,64 @@
 package usecase
+
+import (
+	"github.com/stretchr/testify/assert"
+	"gojek-1st/pkg/driver/mocks"
+	"gojek-1st/pkg/entity"
+	"testing"
+)
+
+const (
+	testLat  = 3.601034
+	testLong = 98.679148
+)
+
+func Test_DriverService(t *testing.T) {
+	t.Run("GetDriverWithinLatLngBounds", GetDriverWithinLatLngBounds_should_get_correct_info)
+}
+
+func CreateTestDriver1() entity.Driver {
+	test_driver_1 := entity.Driver{
+		Accuracy: 0.7,
+		Lat:      3.607715,
+		Long:     98.672960,
+		Id:       2,
+	}
+	return test_driver_1
+}
+
+func CreateTestDriver2() entity.Driver {
+	return entity.Driver{
+		Accuracy: 0.8,
+		Lat:      3.602576,
+		Long:     98.681196,
+		Id:       3,
+	}
+}
+
+func CreateTestDriver3() entity.Driver {
+	return entity.Driver{
+		Accuracy: 0.7,
+		Lat:      3.606002,
+		Long:     98.843245,
+		Id:       4,
+	}
+}
+
+func GetDriverWithinLatLngBounds_should_get_correct_info(t *testing.T) {
+
+	driver1 := CreateTestDriver1()
+	driver2 := CreateTestDriver2()
+	driver3 := CreateTestDriver3()
+	mockRepo := new(mocks.Repository)
+	allDrivers := []*entity.Driver{&driver1, &driver2, &driver3}
+	expectedDrivers := []*entity.Driver{&driver1, &driver2}
+	mockRepo.On("GetAll").Return(allDrivers, nil)
+	driverUcase := NewDriverUsecase(mockRepo)
+	actualDrivers, err := driverUcase.FindDrivers(testLat, testLong, 1100, 10)
+	if err != nil {
+		t.Errorf("Unable to find drivers: %s", err.Error())
+	}
+	assert.ElementsMatch(t, expectedDrivers, actualDrivers, "Expected drivers and the actual drivers are "+
+		"not the same!")
+
+}
