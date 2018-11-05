908db714a182bff067b884777f7c96de6003071f
Anthony Tjuatja <anthonytjuatja@gmail.com>
2018-11-06 00:36:14 +0700

    Implemented UpdateDriver handler

--- controller/handler/driver.go
+++ controller/handler/driver.go
@@ -1,9 +1,13 @@
 package handler
 
 import (
+	"encoding/json"
 	"github.com/gorilla/mux"
 	"gojek-1st/pkg/driver"
+	"gojek-1st/pkg/entity"
+	"log"
 	"net/http"
+	"strconv"
 )
 
 const (
@@ -13,7 +17,41 @@ const (
 
 func UpdateDriver(driverUsecase driver.Usecase) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		errorMessage := "Error updating driver"
+		vars := mux.Vars(r)
+		var d *entity.Driver
+
+		err := json.NewDecoder(r.Body).Decode(&d)
+		if err != nil {
+			writeError(&w, err, errorMessage)
+		}
+		id, err := strconv.ParseInt(vars["id"], 10, 32)
+		d.Id = int32(id)
+		if err != nil {
+			writeError(&w, err, errorMessage)
+		}
+
+		d.Id, err = driverUsecase.UpdateLocation(d.Id, d.Lat, d.Long, d.Accuracy)
+
+		if err != nil {
+			writeError(&w, err, errorMessage)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+		if err := json.NewEncoder(w).Encode(d); err != nil {
+			writeError(&w, err, errorMessage)
+			return
+		}
+
 	})
+}
+
+func writeError(w *http.ResponseWriter, error error, errorTag string) {
+	writer := *w
+	log.Println(error.Error())
+	writer.WriteHeader(http.StatusInternalServerError)
+	writer.Write([]byte(errorTag))
+	return
 }
 
 func GetDrivers(driverUsecase driver.Usecase) http.Handler {
