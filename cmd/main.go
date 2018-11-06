525433ae840a9dabf5cf0d813d783ebc1ae7f97e
Anthony Tjuatja <anthonytjuatja@gmail.com>
2018-11-06 14:19:24 +0700

    Added main.go file and dev_config file

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,43 @@
 package cmd
+
+import (
+	"github.com/gorilla/context"
+	"github.com/gorilla/mux"
+	"gojek-1st/config"
+	"gojek-1st/controller/handler"
+	"gojek-1st/pkg/driver/repository"
+	"gojek-1st/pkg/driver/usecase"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+)
+
+func main() {
+	mongoClient, err := repository.CreateMongoClient()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	mongoRepo := repository.CreateMongoRepository(mongoClient, config.MONGODB_DB_NAME)
+	driverUCase := usecase.NewDriverUsecase(mongoRepo)
+	r := mux.NewRouter()
+	handler.MakeDriverHandlers(r, driverUCase)
+	http.Handle("/", r)
+
+	logger := log.New(os.Stderr, "logger: ", log.Lshortfile)
+	srv := &http.Server{
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		Addr:         ":" + strconv.Itoa(config.REST_API_PORT),
+		Handler:      context.ClearHandler(http.DefaultServeMux),
+		ErrorLog:     logger,
+	}
+
+	err = srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+}
---
+++ config/dev_config.go
@@ -0,0 +1,7 @@
+package config
+
+const (
+	MONGODB_HOST    = "mongodb://localhost:27017"
+	MONGODB_DB_NAME = "gojek-db"
+	REST_API_PORT   = 8080
+)
